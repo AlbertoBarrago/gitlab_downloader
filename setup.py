2c5d0d07ff48cb41a212e159708ffb9e9f84acf9
Alberto Barrago <albertobarrago@gmail.com>
2024-10-24 16:13:26 +0200

    Updated pipe

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", encoding="utf-8") as f:
 
 setup(
     name="gitlab_projects_downloader",
-    version="0.3.0",
+    version="0.4.0",
     packages=find_packages(),
     install_requires=[
         "requests",
--- test/test_gitlab_exporter.py
+++ test/test_gitlab_exporter.py
@@ -1,14 +1,25 @@
+"""
+Test for gitlab_exporter.py
+"""
 import os
 import unittest
 from unittest.mock import Mock, patch
-from gitlab_downloader import GitlabExporter
+
+from gitlab_downloader.main import GitlabExporter
 
 headers = {
     "Private-Token": "1234"
 }
 
+
 class TestExportProject(unittest.TestCase):
+    """
+    Test exporter class
+    """
     def setUp(self):
+        """
+        Setup process
+        """
         self.project_name = "test_project"
         self.export_folder = "./gitlab_exports"
         self.gitlab_exporter = GitlabExporter(self.project_name)
@@ -17,6 +28,9 @@ class TestExportProject(unittest.TestCase):
             os.makedirs(self.export_folder)
 
     def tearDown(self):
+        """
+        Tear Down process
+        """
         if os.path.exists(self.export_folder):
             for file in os.listdir(self.export_folder):
                 os.remove(os.path.join(self.export_folder, file))
@@ -26,15 +40,22 @@ class TestExportProject(unittest.TestCase):
     @patch('requests.get')
     @patch('builtins.open', new_callable=unittest.mock.mock_open)
     def test_export_project_success(self, mock_open, mock_get, mock_post):
-        # Mock the initiation of the export
+        """
+        Test export project success
+        """
         mock_post.return_value = Mock(status_code=202)
 
-        # Mock the responses for project retrieval and status
         mock_get.side_effect = [
-            Mock(status_code=200, json=lambda: [{"id": 1, "name": "test_project", "path_with_namespace": "namespace/test_project"}]),
+            Mock(status_code=200,
+                 json=lambda: [{"id": 1, "name": "test_project",
+                                "path_with_namespace": "namespace/test_project"}]),
             Mock(status_code=200, json=lambda: {"export_status": "queued"}),
-            Mock(status_code=200, json=lambda: {"export_status": "finished", "_links": {"api_url": "http://download_url"}}),
-            Mock(status_code=200, iter_content=lambda chunk_size: [b'content_chunk1', b'content_chunk2', b'content_chunk3'])  # Simulate chunked content
+            Mock(status_code=200,
+                 json=lambda: {"export_status": "finished",
+                               "_links": {"api_url": "http://download_url"}}),
+            Mock(status_code=200,
+                 iter_content=lambda chunk_size: [b'content_chunk1',
+                                                  b'content_chunk2', b'content_chunk3'])
         ]
 
         # Call the function to export the project
@@ -53,6 +74,7 @@ class TestExportProject(unittest.TestCase):
         # Ensure correct API calls were made
         mock_post.assert_called_once()
         self.assertEqual(mock_get.call_count, 4)
+
 
 if __name__ == '__main__':
     unittest.main()
